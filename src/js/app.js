444ec72647bbb31b644e73ff917e9d4bd14f893e
nezvit13 <nezdojminoga@gmail.com>
2022-05-18 16:13:22 +0300

    needed some fix bug with video

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,9 @@ import "../scss/app.scss";
 import "./animate.js";
 import "./animatedValue.js";
 import "./scroll-up.js";
-import "./swiper.js"
+import "./burger.js";
+import "./swiper.js";
+import "./video.js";
 /* Your JS Code goes here */
 
 /* Demo JS */
